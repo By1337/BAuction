094f1989af8a382f5f30811400e3c0b52a911040
By1337 <by133700@gmail.com>
2024-08-08 08:17:18 +0300

    refactor: remove BAucApi module

--- BaucApi/src/main/java/org/by1337/bauction/api/ParsePDCTags.java
+++
@@ -1,9 +0,0 @@
-package org.by1337.bauction.api;
-
-import org.bukkit.persistence.PersistentDataContainer;
-
-import java.util.List;
-
-public interface ParsePDCTags {
-    List<String> parseTags(PersistentDataContainer persistentDataContainer);
-}
--- BaucApi/src/main/java/org/by1337/bauction/api/SyncDetector.java
+++
@@ -1,5 +0,0 @@
-package org.by1337.bauction.api;
-@Deprecated(forRemoval = true)
-public interface SyncDetector {
-    boolean isSync();
-}
